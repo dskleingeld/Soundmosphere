3b2d78386385b17b3d2708744dd65c3c3f218adc
dskleingeld <dskleingeldgmail.com>
2019-12-05 21:18:37 +0100

    remember last 90 words spoken for analysis

--- decision_map.py
+++ decision_map.py
@@ -1,14 +1,20 @@
 from queue import Queue as tQueue
 import threading
 
+MAX_RECENT_WORDS = 90
+
 def analyse(rx, playlist_changes):
     t = threading.currentThread()
+    recent_words = []
+    
+
     while getattr(t, "keep_running", True):
         if not rx.poll(timeout=0.1):
             continue
         
         new_text = rx.recv()
-        print(new_text) #do usefull stuff with text instead
-
+        recent_words = recent_words+new_text.split(" ")
+        recent_words = recent_words[-MAX_RECENT_WORDS:]
+        recent_text = " ".join(recent_words)
 
     print("stopped analysis")
