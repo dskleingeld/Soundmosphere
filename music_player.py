bce2da4c14cd3a42f00aa123b69a47c220fcb6b0
dskleingeld <dskleingeldgmail.com>
2019-12-05 14:04:12 +0100

    adds class/enums for controlling music player

--- music_player.py
+++ music_player.py
@@ -1,14 +1,32 @@
 import queue
 import threading
 
+from enum import Enum
+
+class OrderType(Enum):
+    VOLUME_CHANGE = 1
+    SONG_CHANGE = 2
+    #might add new shit
+
+class Change(Enum):
+    FAST = 1
+    SLOW = 2
+    #might add new shit too
+
+class Order(Enum):
+    orderType = OrderType.SONG_CHANGE#can be OrderType
+    path = ""
+    volume = 0
+
 def play(playlist_changes):
     t = threading.currentThread()
+
     while getattr(t, "keep_running", True):
         try:
             changes = playlist_changes.get_nowait()
         except queue.Empty:
             continue
 
-        print(changes)
-    print("stopped music playback")
+        #do something with the changes
 
+    print("stopped music playback")
