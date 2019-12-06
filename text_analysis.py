5d238aab8d430a121abf0ca5aef621057854a6dd
Lisa <lisapothoven@gmail.com>
2019-12-06 08:47:14 +0100

    updated keywords

--- text_analysis.py
+++ text_analysis.py
@@ -1,14 +1,12 @@
-keywords = ["autumn", "mist", "white", "chill", "sun", "shadow", "shadows", "ride", "rode", "grass", "flower", "flowers", "bird", "birds", "tree", "trees", "herd", "herds", "deer", "browse", "browsing", "sit", "sitting", "noon", "shade"]
-energy = [-1, -1, 1, -1, 1, -1, -1, 1, 1, 0, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 0, -1, -1, 1, -1]
-stress = [-1, 0, 0, -1, -1, 1, 1, 0, 0, -1, -1, -1, -1, -1, -1, -1, 0, 0, -1, -1, -1, -1, -1, 0, 0]
+from keywords import keywords, energy, stress
 
 def matchKeyword(word):
   energyval = 0.0
   stressval = 0.0
 
   if word in keywords:
-    energyval = energy[keywords.index(word)]*0.1
-    stressval = stress[keywords.index(word)]*0.1
+    energyval = energy[keywords.index(word)]*0.05
+    stressval = stress[keywords.index(word)]*0.05
 
   return energyval, stressval
 
@@ -16,14 +14,10 @@ def analyseText(text):
   energytotal = 0
   stresstotal = 0
   word = ""
-  for char in text:
-    if char == " ":
-      energyval, stressval = matchKeyword(word)
-      energytotal += energyval
-      stresstotal += stressval
-      word = ""
-    else:
-      word += char
+  for word in text:
+    energyval, stressval = matchKeyword(word)  
+    energytotal += energyval
+    stresstotal += stressval
 
   if energytotal>1: energytotal=1
   elif energytotal<-1: energytotal=-1
